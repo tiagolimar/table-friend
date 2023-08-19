cab20db7754cd9149db766d8bbe79619bbd813bc
tiagolimar <tiagolima.r2@gmail.com>
2023-08-19 01:45:48 -0300

    adicionando estilo

--- table-friend/src/components/MenuBar.jsx
+++ table-friend/src/components/MenuBar.jsx
@@ -1,9 +1,9 @@
 export const MenuBar = () => {
   return (
     <nav className="bg-dark"><ul className="d-flex decoration-none gap-4 p-3 justify-content-end">
-        <li><a className="btn btn-warning" href="#">HOME</a></li>
-        <li><a className="btn btn-warning" href="#">USUÁRIOS</a></li>
-        <li><a className="btn btn-warning" href="#">&#128270;LOGIN</a></li>
+        <li><a className="btn btn-warning" href="/">HOME</a></li>
+        <li><a className="btn btn-warning" href="/usuarios">USUÁRIOS</a></li>
+        <li><a className="btn btn-warning" href="/login">&#128270;LOGIN</a></li>
     </ul></nav>
   )
 }
--- table-friend/src/pages/Home.jsx
+++ table-friend/src/pages/Home.jsx
@@ -6,7 +6,8 @@ function Home() {
             <main className="container d-flex flex-column align-items-center justify-content-center altura-total">
                 <h1>SEJA BEM-VINDO!!!</h1>
                 <h2 className="text-danger">TABLE FRIEND</h2>
-            </main>  
+                <h4 className="text-secondary text-center">TENHO UMAS IDEIAS DO QUE COLOCAR NESSA PÁGINA INICIAL (IMAGENS E TEXTOS SÓ PRA PREENCHER ESSA PÁGINA INICIAL)</h4>
+            </main>
         </>
     );
 }
